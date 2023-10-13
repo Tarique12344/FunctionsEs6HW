5d4d2a891c7180c88fc2b3936454d8bb736b5998
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-10-13 15:55:29 -0400

    Es6 Debugging HW

---
+++ app.js
@@ -0,0 +1,41 @@
+var d = 10
+var c = 5
+// add
+function add(d,c) {
+    return d + c;
+};
+var result= add(d,c);
+console.log(result);
+// sub
+function sub(d,c) {
+    return d + c;
+};
+
+var result= sub(d,c);
+console.log(result);
+
+function multiple(d,c) {
+    return d * c;
+};
+
+var result= multiple(d,c);
+console.log(result);
+
+function divide(d,c) {
+    return d / c;
+};
+
+var result= divide(d,c);
+console.log(result);
+
+var result = (d,c) => (d + c);
+console.log(result(d,c));
+
+var result = (d,c) => (d / c);
+console.log(result(d,c));
+
+var result = (d,c) => (d * c);
+console.log(result(d,c));
+
+var result = (d,c) => (d / c);
+console.log(result(d,c));
